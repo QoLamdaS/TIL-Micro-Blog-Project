aecab5d92c56a8638d5ddf63a8a82ebe7fdb0fc1
QoLamdaS <kaffeeunused@gmail.com>
2026-02-21 13:37:06 +0700

    Finally it works to "print Hello World!" using Python Flask. I really have no idea.

--- The Main Project/app.py
+++ The Main Project/app.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Finally it works for now!!!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
