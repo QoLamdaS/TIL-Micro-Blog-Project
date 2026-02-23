0ee9c09ce9ab756d0904612d9a017f893e9a99ca
QoLamdaS <kaffeeunused@gmail.com>
2026-02-23 19:27:22 +0700

    Testing output again. I have no idea why it can't be open on browser

--- The Main Project/app.py
+++ The Main Project/app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!!!</p>"
+def testing_outputs():
+    return "<h1>This is a test output for the function.</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
