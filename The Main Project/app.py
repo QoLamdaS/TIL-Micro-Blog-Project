1a0bc6a43155d4c49572a95d1f21cc5d1ecf8d93
QoLamdaS <kaffeeunused@gmail.com>
2026-02-24 19:10:54 +0700

    I really have no idea.

--- The Main Project/app.py
+++ The Main Project/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!!!</p>"
+    return "<h1>Hello, World!!!</h1>"
 def testing_outputs():
     return "<h1>This is a test output for the function.</h1>"
 
